1316186b2d2ee263499b54377d200bd1c6cbe018
Bruno Palodetto <brunopalodetto@hotmail.com>
2024-11-29 21:41:27 -0300

    Fix

--- tests/domain.unitTest/UseCase/OrderUseCaseTest.cs
+++ tests/domain.unitTest/UseCase/OrderUseCaseTest.cs
@@ -20,6 +20,7 @@ namespace UseCase.OrderTest
         Mock<IProductRepository> _productRepository;
         Mock<NotificationContext> _notificationContext;
         Mock<IPaymentClient> _paymentClient;
+        Mock<IProductionClient> _productionClient;
 
         Order orderResponseMock;
         Product productResponseMock;
@@ -32,12 +33,14 @@ namespace UseCase.OrderTest
             _productRepository = new Mock<IProductRepository>();
             _notificationContext = new Mock<NotificationContext>();
             _paymentClient = new Mock<IPaymentClient>();
+            _productionClient = new Mock<IProductionClient>();
 
             _orderUseCase = new OrderUseCase(_orderRepository.Object, 
                                             _customerRepository.Object, 
                                             _productRepository.Object, 
                                             _notificationContext.Object,
-                                            _paymentClient.Object
+                                            _paymentClient.Object,
+                                            _productionClient.Object
                                             );
 
 
